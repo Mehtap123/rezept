9de8b7a35c0b861d8e44126ad7d050d96b35ae30
Mehtap <kayhanmehtap10@gmail.com>
2022-04-29 16:10:06 +0200

    fetching Data

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import { client } from './Components/client';
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import Recipe from './Components/Recipe';
 
 function App() {
- 
-  const state = {
-    articles:[]
-  }
 
-  client.getEntries()
-    .then((data) => {console.log(data)})
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = () => {client.getEntries()
+    .then((res) => {setData(res)})
     .catch(console.error)
+    };
+    fetchData();
+  }, []);
+
+  console.log(data)
    
   return (
 
-
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <Routes>
+      <Route path="/" element={<Recipe data={data}/>}/>
+    </Routes>
   </div>
 );
 }
