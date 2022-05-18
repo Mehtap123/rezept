70889d4599634628dae74b818300c1db66ebc2f0
Mehtap <kayhanmehtap10@gmail.com>
2022-05-18 17:51:02 +0200

    connect api & update path

--- src/Components/RecipeInfo.js
+++ src/Components/RecipeInfo.js
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom";
 
 const RecipeInfo = ({data}) => {
+  
   const { id } = useParams();
   const navigate = useNavigate();
 
@@ -8,20 +9,21 @@ const RecipeInfo = ({data}) => {
     navigate("/");
   };
 
-  const foundRecipe = data.find((recipe) => id === recipe.sys.id);
-  const zutaten = foundRecipe.fields.zutaten;
+  const foundRecipe = data.find((recipe) => id == recipe.Recipe_ID);
+  
+  const zutaten = foundRecipe.Ingredients;
 
   return (
     <>
       {foundRecipe && 
         <div className="card recipe-info">
-          <img className="card-img-top image" src={foundRecipe.fields.bild.fields.file.url}/>
+          <img className="card-img-top image" src={foundRecipe.PictureURL}/>
           <div className="card-body">
-            <h1 className="card-title">{foundRecipe.fields.berschrift}</h1>          
+            <h1 className="card-title">{foundRecipe.Title}</h1>          
             <ul>
               {zutaten.map((zutat, index) => <li key={index}>{zutat}</li>)}
             </ul>
-            <p className="card-text">{foundRecipe.fields.zubereitung}</p>
+            <p className="card-text">{foundRecipe.Instructions}</p>
             <button onClick={handleClick} className="btn btn-primary">Home</button>
           </div>
         </div>}
